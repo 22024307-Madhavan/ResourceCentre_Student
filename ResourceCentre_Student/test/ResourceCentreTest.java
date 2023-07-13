ab1ed74725427f37aaa1537e0bb227a26af5533c
22046375 <22046375@DESKTOP-5NKNLI1.mshome.net>
2023-07-13 09:49:40 +0800

    Muhammad Nur-Faiz

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 // C206 AY2023 SEM1 - Eclipse 21-09 JDK16
-
+// Muhammad Nur-Faiz
 public class ResourceCentreTest {
 	// prepare test data
 	private Camcorder cc1;
